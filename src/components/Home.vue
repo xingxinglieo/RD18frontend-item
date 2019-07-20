93e59083583dc4a9933f229e66f9a5562d1ad226
MAKERYI <18898340516@163.com>
2019-07-20 12:26:35 +0800

    新增 主界面组件

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <logo></logo>
+    <user></user>
+    <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+import Logo from './Logo'
+import User from './User'
+import SearchBar from './SearchBar'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    'logo': Logo,
+    'user': User,
+    'search-bar': SearchBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  background: url("../assets/background.jpg") no-repeat;
+  background-attachment: fixed;
+  background-size: 100%;
+}
+
+</style>
