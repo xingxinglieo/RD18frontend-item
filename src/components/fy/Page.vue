7d2f5e028971a171080b253b172a07cc749ef9c1
Puluman <742751180@qq.com>
2019-07-24 20:29:35 +0800

    7.24

---
+++ src/components/fy/Page.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="page">
+		<person></person>
+		<div class="photo-wall">
+			<photo></photo>
+			<photo></photo>
+			<photo></photo>
+			<photo></photo>
+			<photo></photo>
+		</div>
+	</div>
+</template>
+
+
+<script>
+
+import Photo from './Photo'
+import Person from './Person'
+
+ export default {
+	name: "page",
+	components: {
+		'photo': Photo,
+		'person': Person
+	}
+ }
+
+</script>
+
+
+<style>
+#app .page {
+	text-align: start;
+	background: rgb(186, 210, 233);
+	height: 100%;
+	z-index: -77;
+}
+
+.page:after {
+	content: "";
+	clear: both;
+	display: block;
+}
+
+.photo-wall{
+	margin: 0 auto;
+	padding: 40px 60px;
+	width: 800px;
+	height: 880px;
+	background: #f9faf3;
+	border: 0.5px solid #dedede;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+/*import HelloWorld from '@/components/HelloWorld'*/
+
+import Page from '@/components/fy/Page'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }*/
+
+    {
+      path: '/page',
+      component: Page
     }
-  ]
+  ],
+  mode: 'history'
 })
