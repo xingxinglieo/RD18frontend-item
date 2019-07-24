60b89f8611ea93d0d861a727a610642d6bc85c65
unknown <you@example.com>
2019-07-24 22:19:46 +0800

    7.24

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,23 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
+	export default {
+		name: 'App',
+		data() {
+			return {
 
-<<<<<<< HEAD
-
-export default {
-  name: 'App',
-  data(){
-    return{
-
-    }
-  }
-=======
-export default {
-  name: 'App',
-  components: {
-  }
-  
->>>>>>> 55360c9fdffad5670d0e128250a1524911481692
-}
+			}
+		}
+	}
 </script>
 
 <style>
-<<<<<<< HEAD
-*{
-	margin: 0px;
-	padding: 0px;
-}
-=======
-<<<<<<< HEAD
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-=======
->>>>>>> 55360c9fdffad5670d0e128250a1524911481692
-
->>>>>>> eb21cc9ce39ee0f43beee6cd53141eb6326fae41
+	* {
+		margin: 0px;
+		padding: 0px;
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 /*import HelloWorld from '@/components/HelloWorld'*/
-
 import Page from '@/components/fy/Page'
-
-=======
 import HelloWorld from '@/components/HelloWorld'
 import logincontain from '@/components/Login/LoginContain'
->>>>>>> 55360c9fdffad5670d0e128250a1524911481692
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-<<<<<<< HEAD
-    /*{
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }*/
-
-    {
-      path: '/page',
-      component: Page
-    }
-  ],
-  mode: 'history'
-=======
-    {path: '/',name: 'HelloWorld',component: HelloWorld},
-	{path:'/login',component:logincontain},
-  ]
->>>>>>> 55360c9fdffad5670d0e128250a1524911481692
+	routes: [{
+			path: '/',
+			name: 'HelloWorld',
+			component: HelloWorld
+		},
+		{
+			path: '/login',
+			component: logincontain
+		},
+		{
+			path: '/page',
+			component: Page
+		},
+	],
+	mode: 'history',
 })
