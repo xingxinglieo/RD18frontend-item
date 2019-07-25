f711782460868989c82577b6e50173e2bb24d7cc
MAKERYI <18898340516@163.com>
2019-07-25 09:57:16 +0800

    新增组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home/Home'
 import logincontain from '@/components/Login/LoginContain'
+import AlterUserInfo from '@/components/Alter-info/AlterInfo'
+import AlterMailboxOrPassword from '@/components/Alter-info/AlterMailboxOrPassword'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path: '/',name: 'HelloWorld',component: HelloWorld},
-	{path:'/login',component:logincontain},
-  ]
+    {
+      path: '/',name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path:'/login',
+      component:logincontain
+    },
+    {
+      path:'/home',
+      component: Home
+    },
+    {
+      path:'/alterMyInfo',
+      component: AlterUserInfo
+    },
+    {
+      path:'/alterMailboxOrPassword',
+      component: AlterMailboxOrPassword
+    }
+    
+  ],
+  mode: 'history'
 })
